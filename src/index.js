fa3cce95d9d6d3156c23c53362261ef3591606d5
Akshitgupta45 <akshit.rke@gmail.com>
2023-07-07 12:18:21 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
+// const fileContent = "An online learning platform";
 const updateFile = async (fileName, fileContent) => {
-  // write code here
-  // dont change function name
-  
+	// write code here
+	// dont change function name
+	try {
+		const existingContent = await fs.readFile(filename, "utf-8");
+		const updatedContent = existingContent + "," + fileContent;
+		await fs.appendFile(fileName, updatedContent);
+		console.log(`FILE ${fileName} CREATED SUCCESFULLY`);
+	} catch (error) {
+		console.error("ERROR WHILE UPDATING THE FILE");
+		return null;
+	}
 };
-
+// console.log(newFile);
 module.exports = updateFile;
